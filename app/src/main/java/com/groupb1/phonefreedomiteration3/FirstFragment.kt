540943ef5c3bd5b52f228b9718b4ed19bec44b70
Oliver Klenum <oliverbrinkklenum@gmail.com>
2021-12-27 17:21:53 +0100

    Date Time

--- app/src/main/java/com/groupb1/phonefreedomiteration3/FirstFragment.kt
+++ app/src/main/java/com/groupb1/phonefreedomiteration3/FirstFragment.kt
@@ -5,10 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.TimePicker
-import android.widget.Toast
+import android.widget.*
 import androidx.navigation.Navigation
 import java.util.*
 
@@ -18,11 +15,10 @@ import java.util.*
  * create an instance of this fragment.
  */
 class FirstFragment : Fragment() {
-    var day = 0
-    var month = 0
-    var year = 0
-    var hour = 0
-    var minute = 0
+    lateinit var timePicker: TimePickerHelper
+    lateinit var datePicker: DatePickerHelper
+    private lateinit var timeTextView: TextView
+    private lateinit var dateTextView: TextView
 
 
     override fun onCreateView(
@@ -32,22 +28,50 @@ class FirstFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_first, container, false)
 
+        datePicker = DatePickerHelper(this.requireContext())
+        timePicker = TimePickerHelper(this.requireContext(), true, false)
+        timeTextView = view.findViewById(R.id.timeView)
+        dateTextView = view.findViewById(R.id.dateView)
+        val timeButton = view.findViewById<ImageButton>(R.id.selectTimeBtn)
+        val dateButton = view.findViewById<ImageButton>(R.id.selectDateBtn)
+        timeButton.setOnClickListener {
+            showTimePickerDialog()
+        }
+        dateButton.setOnClickListener {
+            showDatePickerDialog()
+        }
+
         val activateButton = view.findViewById<Button>(R.id.activateButton)
-        activateButton.setOnClickListener {Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment)
-
-        pickDate()
-        }
-
-        private fun getTime() {
-            val cal: Calendar.getInstance()
-            hour = cal.get(Calendar.HOUR)
-        }
+        activateButton.setOnClickListener {Navigation.findNavController(view).navigate(R.id.action_firstFragment_to_secondFragment) }
 
         return view
     }
 
-    override fun onDateSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
-
+    private fun showTimePickerDialog() {
+        val cal = Calendar.getInstance()
+        val h = cal.get(Calendar.HOUR_OF_DAY)
+        val m = cal.get(Calendar.MINUTE)
+        timePicker.showDialog(h, m, object : TimePickerHelper.Callback {
+            override fun onTimeSelected(hourOfDay: Int, minute: Int) {
+                val hourStr = if (hourOfDay < 10) "0${hourOfDay}" else "${hourOfDay}"
+                val minuteStr = if (minute < 10) "0${minute}" else "${minute}"
+                timeTextView.text = "${hourOfDay}:${minuteStr}"
+            }
+        })
+    }
+    private fun showDatePickerDialog() {
+        val cal = Calendar.getInstance()
+        val d = cal.get(Calendar.DAY_OF_MONTH)
+        val m = cal.get(Calendar.MONTH)
+        val y = cal.get(Calendar.YEAR)
+        datePicker.showDialog(d, m, y, object : DatePickerHelper.Callback {
+            override fun onDateSelected(dayofMonth: Int, month: Int, year: Int) {
+                val dayStr = if (dayofMonth < 10) "0${dayofMonth}" else "${dayofMonth}"
+                val mon = month + 1
+                val monthStr = if (mon < 10) "0${mon}" else "${mon}"
+                dateTextView.text = "${dayStr}-${monthStr}-${year}"
+            }
+        })
     }
 
 
---
+++ app/src/main/java/com/groupb1/phonefreedomiteration3/TimeSelector.kt
@@ -0,0 +1,39 @@
+package com.groupb1.phonefreedomiteration3
+
+import android.app.TimePickerDialog
+import android.app.TimePickerDialog.OnTimeSetListener
+import android.content.Context
+import java.util.*
+
+class TimePickerHelper(
+    context: Context,
+    is24HourView: Boolean,
+    isSpinnerType: Boolean = false
+) {
+
+    private var dialog: TimePickerDialog
+    private var callback: Callback? = null
+
+    private val listener = OnTimeSetListener { timePicker, hourOfDay, minute ->
+        callback?.onTimeSelected(hourOfDay, minute)
+    }
+
+    init {
+        val style = if (isSpinnerType) R.style.SpinnerTimePickerDialog else 0
+
+        val cal = Calendar.getInstance()
+
+        dialog = TimePickerDialog(context, style, listener,
+            cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), is24HourView)
+    }
+
+    fun showDialog(hourOfDay: Int, minute: Int, callback: Callback?) {
+        this.callback = callback
+        dialog.updateTime(hourOfDay, minute)
+        dialog.show()
+    }
+
+    interface Callback {
+        fun onTimeSelected(hourOfDay: Int, minute: Int)
+    }
+}
