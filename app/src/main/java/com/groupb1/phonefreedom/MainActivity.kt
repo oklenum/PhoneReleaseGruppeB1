c0dce211a4a47040da89c584aa31ea86eb252f76
Oliver Klenum <oliverbrinkklenum@gmail.com>
2022-01-10 14:42:44 +0100

    test permissions 1.01

--- app/src/main/java/com/groupb1/phonefreedom/MainActivity.kt
+++ app/src/main/java/com/groupb1/phonefreedom/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.groupb1.phonefreedom
 
+import android.Manifest
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,41 +11,44 @@ import androidx.core.content.ContextCompat
 import com.groupb1.phonefreedom.R
 
 class MainActivity : AppCompatActivity() {
-    private val DND_PERMISSION_CODE = 123
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         findViewById<Button>(R.id.goBtn).setOnClickListener {
-            checkPermission(
-                android.Manifest.permission.ACCESS_NOTIFICATION_POLICY,
-                DND_PERMISSION_CODE
-            )
+            checkPermission(Manifest.permission.ACCESS_NOTIFICATION_POLICY, 1)
         }
     }
 
     private fun checkPermission(permission: String, requestCode: Int) {
-        if (ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED) {
-            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+        if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_NOTIFICATION_POLICY) !== PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity, Manifest.permission.ACCESS_NOTIFICATION_POLICY)) {
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_NOTIFICATION_POLICY), 1)
+            } else {
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_NOTIFICATION_POLICY), 1)
 
-        } else {
-            Toast.makeText(this@MainActivity, "Permission Granted Already", Toast.LENGTH_LONG).show()
-
+            }
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == DND_PERMISSION_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this@MainActivity, "ACCESS_NOTIFICATION_POLICY Permission Granted", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this@MainActivity, "ACCESS_NOTIFICATION_POLICY Permission Denied", Toast.LENGTH_SHORT).show()
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    if ((ContextCompat.checkSelfPermission(this@MainActivity,
+                            Manifest.permission.ACCESS_NOTIFICATION_POLICY) ===
+                                PackageManager.PERMISSION_GRANTED)) {
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
             }
         }
     }
