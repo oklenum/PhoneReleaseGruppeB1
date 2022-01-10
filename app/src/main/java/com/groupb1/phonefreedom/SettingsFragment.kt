14869bebd7cce1699c2bab1b43d469222f2c5987
Oliver Klenum <oliverbrinkklenum@gmail.com>
2022-01-10 12:18:04 +0100

    settings initial

--- app/src/main/java/com/groupb1/phonefreedom/SettingsFragment.kt
+++ app/src/main/java/com/groupb1/phonefreedom/SettingsFragment.kt
@@ -1,11 +1,23 @@
 package com.groupb1.phonefreedom
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
