d37e58475b452777f157a8bd06222f82ad04ee96
Oliver Klenum <oliverbrinkklenum@gmail.com>
2022-01-10 14:44:03 +0100

    test permissions 1.02

--- app/src/main/java/com/groupb1/phonefreedom/MainActivity.kt
+++ app/src/main/java/com/groupb1/phonefreedom/MainActivity.kt
@@ -2,10 +2,12 @@ package com.groupb1.phonefreedom
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.groupb1.phonefreedom.R
@@ -13,17 +15,19 @@ import com.groupb1.phonefreedom.R
 class MainActivity : AppCompatActivity() {
 
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.goBtn).setOnClickListener {
-            checkPermission(Manifest.permission.ACCESS_NOTIFICATION_POLICY, 1)
+            this.checkPermission(Manifest.permission.ACCESS_NOTIFICATION_POLICY, 1)
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun checkPermission(permission: String, requestCode: Int) {
-        if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_NOTIFICATION_POLICY) !== PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_NOTIFICATION_POLICY) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity, Manifest.permission.ACCESS_NOTIFICATION_POLICY)) {
                 ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.ACCESS_NOTIFICATION_POLICY), 1)
             } else {
@@ -41,7 +45,7 @@ class MainActivity : AppCompatActivity() {
                 if (grantResults.isNotEmpty() && grantResults[0] ==
                     PackageManager.PERMISSION_GRANTED) {
                     if ((ContextCompat.checkSelfPermission(this@MainActivity,
-                            Manifest.permission.ACCESS_NOTIFICATION_POLICY) ===
+                            Manifest.permission.ACCESS_NOTIFICATION_POLICY) ==
                                 PackageManager.PERMISSION_GRANTED)) {
                         Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
                     }
