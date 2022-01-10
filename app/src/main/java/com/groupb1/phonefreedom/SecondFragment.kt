76c5d8377b2d242c9f8f5a2360623fbe13b8d8c5
s205347 <s205347@student.dtu.dk>
2022-01-10 13:27:12 +0100

    Permission requests

--- app/src/main/java/com/groupb1/phonefreedom/SecondFragment.kt
+++ app/src/main/java/com/groupb1/phonefreedom/SecondFragment.kt
@@ -1,12 +1,18 @@
 package com.groupb1.phonefreedom
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
+import java.util.jar.Manifest
 
 /**
  * A simple [Fragment] subclass.
@@ -15,6 +21,10 @@ import androidx.navigation.Navigation
  */
 class SecondFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    lateinit var BtnDND: Button
+    private val DND_PERMISSION_CODE = 123
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,12 +33,6 @@ class SecondFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_second, container, false)
 
-        val activateButton = view.findViewById<ImageButton>(R.id.deactivateButton)
-        activateButton.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_secondFragment_to_firstFragment)
-        }
-
         return view
     }
-
 }
---
+++ app/src/main/java/permission/MainActivity1.kt
@@ -0,0 +1,52 @@
+package Permission
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.Navigation
+import com.groupb1.phonefreedom.R
+
+class MainActivity1 : AppCompatActivity() {
+    private val DND_PERMISSION_CODE = 123
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main1)
+
+        checkPermission(android.Manifest.permission.ACCESS_NOTIFICATION_POLICY, DND_PERMISSION_CODE)
+
+    }
+
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity1, permission) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this@MainActivity1, arrayOf(permission), requestCode)
+
+        } else {
+            Toast.makeText(this@MainActivity1, "Permission Granted Already", Toast.LENGTH_LONG).show()
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == DND_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity1, "ACCESS_NOTIFICATION_POLICY Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity1, "ACCESS_NOTIFICATION_POLICY Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+}
+
