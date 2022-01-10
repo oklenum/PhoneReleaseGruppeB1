703656fd4bc558ae9e1529b81c7b7430e8daa639
Oliver Klenum <oliverbrinkklenum@gmail.com>
2022-01-10 15:20:59 +0100

    test permissions 1.03 rollback, continue work on SebBranch

--- app/src/main/java/com/groupb1/phonefreedom/SecondFragment.kt
+++ app/src/main/java/com/groupb1/phonefreedom/SecondFragment.kt
@@ -33,6 +33,11 @@ class SecondFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_second, container, false)
 
+        val activateButton = view.findViewById<ImageButton>(R.id.deactivateButton)
+        activateButton.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+
         return view
     }
 }
