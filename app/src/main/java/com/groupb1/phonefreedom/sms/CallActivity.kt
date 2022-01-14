81832853f53939f38f663701dc142a92a74862df
s205347 <s205347@student.dtu.dk>
2022-01-14 11:28:56 +0100

    autoReplyCall() created so a sms will be sent when receiving a call

--- app/src/main/java/com/groupb1/phonefreedom/sms/CallActivity.kt
+++ app/src/main/java/com/groupb1/phonefreedom/sms/CallActivity.kt
@@ -1,7 +1,16 @@
 package com.groupb1.phonefreedom.sms
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.database.Cursor
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CallLog
+import android.provider.Telephony
 import com.groupb1.phonefreedom.R
 
 class CallActivity : AppCompatActivity() {
@@ -12,5 +21,19 @@ class CallActivity : AppCompatActivity() {
         setContentView(R.layout.activity_call)
 
         smsManager.checkPermissionSMS()
+        autoReplyCall()
+
+    }
+
+    private fun autoReplyCall() {
+        var br = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    smsManager.phoneNumber = CallLog.Calls.NUMBER
+                    smsManager.sendSMS(smsManager.phoneNumber)
+                }
+            }
+        }
+        registerReceiver(br, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
     }
 }
