00732b14e161be1aec42adb8a0aa0eceef37f3f4
juste99 <Juste99@live.dk>
2022-01-18 16:04:06 +0100

    Call autoReply needs better implementation

--- app/src/main/java/com/groupb1/phonefreedom/appManager/AutoReplyManager.kt
+++ app/src/main/java/com/groupb1/phonefreedom/appManager/AutoReplyManager.kt
@@ -10,15 +10,21 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.provider.CallLog
 import android.provider.Telephony
+import android.telephony.PhoneStateListener
 import android.telephony.SmsManager
+import android.telephony.TelephonyManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.groupb1.phonefreedom.sms.SmsActivity
 
+
 open class AutoReplyManager: AppCompatActivity() {
 
     var phoneNumber = ""
+    var phoneNumber1 = ""
     var smsText = ""
     private var mContext: Context? = null
     private var mActivity: Activity? = null
@@ -31,9 +37,9 @@ open class AutoReplyManager: AppCompatActivity() {
         mActivity = this@AutoReplyManager
 
         checkPermissionSMS()
-        autoReply()
+        autoReplyOnSMS()
+        autoReplyOnCall()
         finish()
-
     }
 
     fun checkPermissionSMS() {
@@ -41,12 +47,21 @@ open class AutoReplyManager: AppCompatActivity() {
             ActivityCompat.checkSelfPermission(this,android.Manifest.permission.RECEIVE_SMS)!= PackageManager.PERMISSION_GRANTED ||
             ActivityCompat.checkSelfPermission(this,android.Manifest.permission.READ_SMS)!= PackageManager.PERMISSION_GRANTED ||
             ActivityCompat.checkSelfPermission(this,android.Manifest.permission.READ_PHONE_NUMBERS)!= PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(this,android.Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.checkSelfPermission(this,android.Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this,android.Manifest.permission.READ_CALL_LOG)!= PackageManager.PERMISSION_GRANTED )
         {
             ActivityCompat.requestPermissions(
                 this, arrayOf(
                 Manifest.permission.SEND_SMS, Manifest.permission.RECEIVE_SMS,
-                Manifest.permission.READ_PHONE_NUMBERS, Manifest.permission.READ_PHONE_STATE),
+                Manifest.permission.READ_PHONE_NUMBERS, Manifest.permission.READ_PHONE_STATE, Manifest.permission.READ_CALL_LOG),
+                111)
+        }
+    }
+    fun checkPermissionCall() {
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.READ_PHONE_STATE),
                 111)
         }
     }
@@ -61,9 +76,9 @@ open class AutoReplyManager: AppCompatActivity() {
 
     fun sendSMS(phoneNumber: String) {
         var sms = SmsManager.getDefault()
-        sms.sendTextMessage(phoneNumber, "me", "pil af med dig", null, null)
+        sms.sendTextMessage(phoneNumber, "me", "Pil af med dig. Sidder lige og arbejder. Er ved min tlf igen kl 17 \n Download Phone Release at xxxxxxx ", null, null)
     }
-    private fun autoReply(){
+    private fun autoReplyOnSMS(){
         var br = object: BroadcastReceiver() {
             override fun onReceive(context: Context?, intent: Intent?) {
                 if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -75,6 +90,28 @@ open class AutoReplyManager: AppCompatActivity() {
             }
         }
         registerReceiver(br, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
+    }
+    private fun autoReplyOnCall(){
+        var br = object: BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                var number = getRecentCallerNumber()
+                if (number != null) {
+                    sendSMS(number)
+                }
+            }
+        }
+        registerReceiver(br, IntentFilter("android.intent.action.PHONE_STATE"))
+    }
+    private fun getRecentCallerNumber(): String? {
+        val managedCursor = contentResolver.query(
+            CallLog.Calls.CONTENT_URI,
+            null, null, null, null
+        )
+        val number = managedCursor?.getColumnIndex(CallLog.Calls.NUMBER)
+        managedCursor?.moveToLast()
+        val phoneNumber = number?.let { managedCursor?.getString(it) }
+
+        return phoneNumber
     }
 
     fun editAutoReply() {
